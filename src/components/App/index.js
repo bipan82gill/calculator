bc74d61dc4e11afeaabd4ae7ed27790d3cfc4b6e
bipan82gill <56560207+bipan82gill@users.noreply.github.com>
2021-01-06 16:21:20 -0500

    create buttons

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -7,7 +7,30 @@ const App = ()=>{
     return <div className="App">
         <div className="top">4:43</div>
         <div className="display">0</div>
-        <div className="buttons"><Button/></div>
+        <div className="buttons">
+            <Button content="AC"/>
+            <Button content="±"/>
+            <Button content="%"/>
+            <Button content="÷"/>
+            <Button content="7"/>
+            <Button content="8"/>
+            <Button content="9"/>
+            <Button content="×"/>
+            <Button content="4"/>
+            <Button content="5"/>
+            <Button content="6"/>
+            <Button content="−"/>
+            <Button content="1"/>
+            <Button content="2"/>
+            <Button content="3"/>
+            <Button content="+"/>
+            <Button content="0"/>
+            <Button content="."/>
+            <Button content="="/>
+
+
+        </div>
+       
         <div className="bottom">----</div>
 
     </div>
--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Button.css';
 
-const Button = (props) =>{
-    return <div className="Button">AC</div>
+const Button = ({content}) =>{
+    return <div className ={`Button ${content === '0'? 'zero' : ''}`}>{content}</div>
 }
 export default Button;
