06aeeb78b7a9f5285774de92dca75f9a65062f56
bipan82gill <56560207+bipan82gill@users.noreply.github.com>
2021-01-06 15:49:58 -0500

    structure calculator

---
+++ src/components/App/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Button from '../Button';
+import './App.css';
+
+const App = ()=>{
+    return <div className="App">
+        <div className="top">4:43</div>
+        <div className="display">0</div>
+        <div className="buttons"><Button/></div>
+        <div className="bottom">----</div>
+
+    </div>
+
+}
+export default App;
